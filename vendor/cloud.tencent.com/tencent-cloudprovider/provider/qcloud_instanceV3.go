4f6f5d13c987d1de2feef90caec53e403692fef8
devinyan <devinyan@tencent.com>
2019-11-13 16:00:26 +0800

    cbs yunnapi v3支持从hostname查询

--- vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_instanceV3.go
+++ vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_instanceV3.go
@@ -18,14 +18,28 @@ package qcloud
 
 import (
 	cvmv3 "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cvm/v20170312"
+	"github.com/golang/glog"
 )
 
 var (
-	FilterNamePrivateIpAddress  = "private-ip-address"
-	FilterNameVpcID = "vpc-id"
+	FilterNamePrivateIpAddress = "private-ip-address"
+	FilterNameVpcID            = "vpc-id"
 )
 
-func (qcloud *QCloud) getInstanceInfoByNodeNameV3(lanIP string) (*cvmv3.Instance, error) {
+func (qcloud *QCloud) getInstanceInfoByNodeNameV3(nodeName string) (*cvmv3.Instance, error) {
+	if qcloud.Config.NodeNameType != HostNameType {
+		return qcloud.getInstanceInfoByLanIpV3(nodeName)
+	} else {
+		instanceId, err := qcloud.getInstanceIdByNodeName(nodeName)
+		if err != nil {
+			glog.Errorf("getInstanceIdByNodeName failed %s", err.Error())
+			return nil, err
+		}
+		return qcloud.getInstanceInfoByInstanceIdSingleV3(instanceId)
+	}
+}
+
+func (qcloud *QCloud) getInstanceInfoByLanIpV3(lanIP string) (*cvmv3.Instance, error) {
 	vpcIdFilter := cvmv3.Filter{&FilterNameVpcID, []*string{&qcloud.Config.VpcId}}
 	privateIPFilter := cvmv3.Filter{&FilterNamePrivateIpAddress, []*string{&lanIP}}
 	descRequest := cvmv3.NewDescribeInstancesRequest()
@@ -45,3 +59,30 @@ func (qcloud *QCloud) getInstanceInfoByNodeNameV3(lanIP string) (*cvmv3.Instance
 	return nil, QcloudInstanceNotFound
 }
 
+func (qcloud *QCloud) getInstanceInfoByInstanceIdSingleV3(instanceId string) (*cvmv3.Instance, error) {
+
+	descRequest := cvmv3.NewDescribeInstancesRequest()
+	descRequest.InstanceIds = []*string{&instanceId}
+
+	glog.V(2).Infof("getInstanceInfoByInstanceIdSingleV3 instanceId %s descRequest %s", instanceId, descRequest.ToJsonString())
+
+	resp, err := qcloud.cvmV3.DescribeInstances(descRequest)
+	if err != nil {
+		glog.Errorf("DescribeInstances failed %s", err.Error())
+		return nil, err
+	}
+
+	if (resp == nil) || (resp.Response == nil) {
+		glog.Errorf("DescribeInstances failed resp or resp.Response is empty")
+		return nil, QcloudInstanceNotFound
+	}
+
+	if len(resp.Response.InstanceSet) == 0 {
+		glog.Errorf("DescribeInstances failed not found %s instance", instanceId)
+		return nil, QcloudInstanceNotFound
+	}
+
+	glog.V(2).Infof("getInstanceInfoByInstanceIdSingleV3 instanceId %s Response %s", instanceId, resp.ToJsonString())
+
+	return resp.Response.InstanceSet[0], nil
+}
