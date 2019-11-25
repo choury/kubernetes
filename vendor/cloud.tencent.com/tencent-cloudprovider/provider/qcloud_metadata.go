709946f0f13a7c87838a7c59117c1b9962205447
devinyan <devinyan@tencent.com>
2019-11-25 19:29:32 +0800

    medata缓存增加过期时间，默认为15分钟

--- vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_metadata.go
+++ vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_metadata.go
@@ -17,8 +17,18 @@ limitations under the License.
 package qcloud
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/dbdd4us/qcloudapi-sdk-go/metadata"
 	glog "k8s.io/klog"
+)
+
+const (
+	EXPIRE_TIME_SECOND_NAME    = "ExpireTimeSecond"
+	DEFAULT_EXPIRE_TIME_SECOND = 15 * 60
 )
 
 //避免对metadata服务的强依赖
@@ -30,49 +40,96 @@ type metaDataCached struct {
 	instanceId  string
 	privateIPv4 string
 	publicIPv4  *string // 可能为nil
+
+	instanceIdLastUpdateTime  time.Time
+	publicIPv4LastUpdateTime  time.Time
+	privateIPv4LastUpdateTime time.Time
+	expireTimeSecond          int64
 }
 
 func newMetaDataCached() *metaDataCached {
+
+	var expireTimeSecond = int64(DEFAULT_EXPIRE_TIME_SECOND)
+
+	if envStr := os.Getenv(EXPIRE_TIME_SECOND_NAME); envStr != "" {
+		glog.Infof("EXPIRE_TIME_SECOND_NAME: %s env is %s ", EXPIRE_TIME_SECOND_NAME, envStr)
+		value, err := strconv.ParseInt(envStr, 10, 64)
+		if err != nil {
+			glog.Warningf("EXPIRE_TIME_SECOND_NAME envStr %s transfer failed,err:%s", envStr, err.Error())
+		} else {
+			if value > 0 {
+				expireTimeSecond = value
+			}
+		}
+	} else {
+		glog.Infof("EXPIRE_TIME_SECOND_NAME: %s env is  empty ", EXPIRE_TIME_SECOND_NAME)
+	}
+
+	glog.Infof("expireTimeSecond %d", expireTimeSecond)
+
 	return &metaDataCached{
-		metaData: metadata.NewMetaData(nil),
+		metaData:         metadata.NewMetaData(nil),
+		expireTimeSecond: expireTimeSecond,
 	}
 }
 
 func (cached *metaDataCached) InstanceID() (string, error) {
-	if cached.instanceId != "" {
+	if (cached.instanceId != "") &&
+		cached.instanceIdLastUpdateTime.Add(time.Duration(cached.expireTimeSecond)*time.Second).After(time.Now()) {
 		return cached.instanceId, nil
 	}
+
 	rsp, err := cached.metaData.InstanceID()
 	if err != nil {
+		glog.Errorf("metaData.InstanceID() get err :%s", err.Error())
 		return "", err
 	}
+
 	cached.instanceId = rsp
+	cached.instanceIdLastUpdateTime = time.Now()
+
 	return cached.instanceId, nil
 }
 
 func (cached *metaDataCached) PrivateIPv4() (string, error) {
-	if cached.privateIPv4 != "" {
+	if (cached.privateIPv4 != "") &&
+		cached.privateIPv4LastUpdateTime.Add(time.Duration(cached.expireTimeSecond)*time.Second).After(time.Now()) {
 		return cached.privateIPv4, nil
 	}
+
 	rsp, err := cached.metaData.PrivateIPv4()
 	if err != nil {
+		glog.Errorf("metaData.PrivateIPv4() get err :%s", err.Error())
 		return "", err
 	}
 
+	if rsp == "" {
+		glog.Errorf("metaData.PrivateIPv4() empty")
+		return "", fmt.Errorf("metaData.PrivateIPv4() empty")
+	}
+
 	cached.privateIPv4 = rsp
+	cached.privateIPv4LastUpdateTime = time.Now()
+
 	return cached.privateIPv4, nil
 }
 
 //反回 "" 时，公网IP不存在
 func (cached *metaDataCached) PublicIPv4() (string, error) {
+
+	if (cached.publicIPv4 != nil) &&
+		cached.publicIPv4LastUpdateTime.Add(time.Duration(cached.expireTimeSecond)*time.Second).After(time.Now()) {
+		return *(cached.publicIPv4), nil
+	}
+
 	rsp, err := cached.metaData.PublicIPv4()
 	if err != nil {
-		if cached.publicIPv4 == nil {
-			return "", err
-		}
-		glog.Warningf("metaData.PublicIPv4() get err :%s, use cached: %s", err, *cached.publicIPv4)
-		return *cached.publicIPv4, nil
+		glog.Errorf("metaData.PublicIPv4() get err :%s", err.Error())
+		return "", err
 	}
+
 	cached.publicIPv4 = &rsp
+	cached.publicIPv4LastUpdateTime = time.Now()
+
 	return *cached.publicIPv4, nil
 }
