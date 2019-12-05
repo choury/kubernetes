e39b1c8a205f698c47d1c3d1dcb3a2c83d85dd89
devinyan <devinyan@tencent.com>
2019-12-05 15:44:17 +0800

    metadata privateIp and instanceId not expired, provider-plugin set log level 3

--- vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_instance.go
+++ vendor/cloud.tencent.com/tencent-cloudprovider/provider/qcloud_instance.go
@@ -136,7 +136,7 @@ func (name kubernetesInstanceID) mapToInstanceID() (string, error) {
 //TODO 如果NodeAddressesByProviderID失败，nodeController会调用此接口
 func (self *QCloud) NodeAddresses(ctx context.Context, name types.NodeName) ([]v1.NodeAddress, error) {
 
-	glog.Infof("QCloud Plugin NodeAddresses NodeName %s", name)
+	glog.V(3).Infof("QCloud Plugin NodeAddresses NodeName %s", name)
 
 	addresses := make([]v1.NodeAddress, 0)
 
@@ -145,15 +145,15 @@ func (self *QCloud) NodeAddresses(ctx context.Context, name types.NodeName) ([]v
 		return nil, err
 	}
 
-	glog.Infof("QCloud Plugin NodeAddresses privateIp %s", privateIp)
+	glog.V(3).Infof("QCloud Plugin NodeAddresses privateIp %s", privateIp)
 
 	addresses = append(addresses, v1.NodeAddress{
 		Type: v1.NodeInternalIP, Address: privateIp,
 	})
 
 	publicIp, err := self.metaData.PublicIPv4()
-	if err == nil && len(publicIp) > 0 {
-		glog.Infof("QCloud Plugin NodeAddresses publicIp %s", publicIp)
+	if err == nil && (publicIp != "") {
+		glog.V(3).Infof("QCloud Plugin NodeAddresses publicIp %s", publicIp)
 		addresses = append(addresses, v1.NodeAddress{
 			Type: v1.NodeExternalIP, Address: publicIp,
 		})
@@ -166,14 +166,14 @@ func (self *QCloud) NodeAddresses(ctx context.Context, name types.NodeName) ([]v
 //只在kubelet中调用
 func (self *QCloud) InstanceID(ctx context.Context, name types.NodeName) (string, error) {
 
-	glog.Infof("QCloud Plugin InstanceID NodeName %s", name)
+	glog.V(3).Infof("QCloud Plugin InstanceID NodeName %s", name)
 
 	instanceId, err := self.metaData.InstanceID()
 	if err != nil {
 		return "", err
 	}
 
-	glog.Infof("QCloud Plugin InstanceID instanceId %s", instanceId)
+	glog.V(3).Infof("QCloud Plugin InstanceID instanceId %s", instanceId)
 
 	zone, err := self.GetZone(ctx)
 	if err != nil {
