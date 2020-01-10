ea26980a62b69d648fee32658c727ca87330f474
howardxiao(肖华源) <howardxiao@tencent.com>
2020-01-10 09:14:36 +0800

    set KubeletReadOnlyPort to 0

--- pkg/master/ports/ports.go
+++ pkg/master/ports/ports.go
@@ -40,7 +40,7 @@ const (
 	// This is necessary for heapster to collect monitoring stats from the kubelet
 	// until heapster can transition to using the SSL endpoint.
 	// TODO(roberthbailey): Remove this once we have a better solution for heapster.
-	KubeletReadOnlyPort = 10255
+	KubeletReadOnlyPort = 0
 	// ProxyHealthzPort is the default port for the proxy healthz server.
 	// May be overridden by a flag at startup.
 	ProxyHealthzPort = 10256
